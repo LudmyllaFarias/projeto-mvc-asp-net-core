b56179730058330df47b278176616058e951dd22
LudmyllaFarias <ludmylla.farias@sysmap.com.br>
2023-02-10 09:28:44 -0300

    Configurando banco de dados

---
+++ ProjetoMvcWeb/ProjetoMvcWeb/Data/BancoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMvcWeb.Models;
+
+namespace ProjetoMvcWeb.Data
+{
+    public class BancoContext : DbContext
+    {
+
+        public DbSet<Contato> contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(@"Data Source=C:\Users\ludmylla.farias\Downloads\SQLiteDatabaseBrowserPortable\contato.db");
+    }
+
+    //Add-Migration InitalCreate
+    // add-migration secondCreate 
+    // update-database
+}
