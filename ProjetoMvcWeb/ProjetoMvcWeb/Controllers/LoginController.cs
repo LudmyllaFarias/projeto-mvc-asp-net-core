cdf4d634689ffa482cb221f2be4a85ef55bd5cca
LudmyllaFarias <ludmylla.farias@sysmap.com.br>
2023-02-13 15:54:29 -0300

    Adiciona view Login

---
+++ ProjetoMvcWeb/ProjetoMvcWeb/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMvcWeb.Models;
+using System;
+
+namespace ProjetoMvcWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if(ModelState.IsValid) 
+                {
+
+                    TempData["MensagemSucesso"] = "Login realizado com sucesso!";
+                    return RedirectToAction("Index", "Home");
+
+                }
+
+                return View("Index");
+            }
+
+            catch(Exception erro)
+            {
+                TempData["MensagemErro"] = $"Houve um erro ao realizar o login! detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
